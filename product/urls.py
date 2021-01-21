41669e92c6208e01b0c38a035d13664023d4cad1
Ohanefk <ohanefk@gmail.com>
2021-01-21 20:19:24 +0600

    added comment logic

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoriesList, ProductViewSet
+from .views import CategoriesList, ProductViewSet, CommentCreate
 
 
 router = DefaultRouter()
@@ -25,4 +25,5 @@ urlpatterns = [
     # path('', products),
     # path('<str:pk>/', products),
     path('', include(router.urls)),
+    path('comments/create/', CommentCreate.as_view()),
 ]
